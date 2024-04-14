5a226e537aa33b527a84313d27a417bed2743827
nguyenvanhau25 <hauuanhh308@gmail.com>
2024-04-15 00:09:35 +0700

    java

---
+++ src/bai2xuatdulieu.java
@@ -0,0 +1,16 @@
+//public class bai2xuatdulieu {
+//    public static void main(String[] args) {
+//        System.out.printf("dong1"); // sout
+//        System.out.println("dong2"); //sout. cách dòng
+//        System.out.println("dong3"); //ctrl d
+
+//        \t: thụt vào 1 tab
+//        \n enter xuống dòng
+//        \" dáu nháy kép
+//        System.out.println("cong hoa ");
+//        System.out.println("\t haha ");
+//        System.out.println(" hau anh \n hahaha");
+//        System.out.println(" \" cho di la con mai\"");
+//
+//    }
+//}
